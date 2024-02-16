89be3493ba3745e1db0181df4c528291c1bb5f35
Ian Blair <54004327+IanBlair07@users.noreply.github.com>
2024-02-16 12:59:25 -0500

    Add files via upload

--- pico_timer/pico_timer.ino
+++ pico_timer/pico_timer.ino
@@ -1,53 +1,64 @@
 
-class Flasher
+class ledStrip
 {
   // Class Member Variables
   // These are initialized in Setup()
+  int ledPin;      // the number of the LED pin
   long OnTime;     // milliseconds of on-time
   long OffTime;    // milliseconds of off-time
 
   // These maintain the current state
   bool timerStatus;               // State used to enable timer
+  int ledState;                 // ledState used to set the LED
   unsigned long previousMillis;   // will store last time change was made
   
   // Constructor - creates a Flasher and initializes the member variables and state
   public:
-  Flasher(bool startStatus) {
+  ledStrip(bool startStatus, int pin) {
     timerStatus = startStatus;
+    ledPin = pin;
+    pinMode(ledPin, OUTPUT);
   }
 
   void Setup(bool newStatus, int newOnTime, int newOffTime){
+    previousMillis = millis();
     OnTime = newOnTime;
     OffTime = newOffTime;
     timerStatus = newStatus;
-  }
+    Serial.begin(115200);
+    Serial.printf("Status: %s, On time: %d ms, Off time: %d ms \n", timerStatus, OnTime,OffTime);
+  } // End of Setup() function
   
-  void Update()
-  {
+  void Update() {
     // check to see if it's time to change the state of the LED
     unsigned long currentMillis = millis();
      
-    if((timerStatus == true) && (currentMillis - previousMillis >= OnTime))
+    if((timerStatus == true) && (ledState == HIGH) && (currentMillis - previousMillis >= OnTime))
     {
       // Turn it off
+      ledState = LOW;  // Turn it off
       previousMillis = currentMillis;  // Remember the time
-      digitalWrite(LED_BUILTIN, LOW);  // Update the actual LED
+      digitalWrite(ledPin, ledState);  // Update the actual LED
+      Serial.printf("Turn off the LED \n");
     }
-    else if ((timerStatus == true) && (currentMillis - previousMillis >= OffTime))
+    else if ((timerStatus == true) && (ledState == LOW) && (currentMillis - previousMillis >= OffTime))
     {
       // turn it on
+      ledState = HIGH;  // Turn it off
       previousMillis = currentMillis;   // Remember the time
-      digitalWrite(LED_BUILTIN, HIGH);   // Update the actual LED
+      digitalWrite(ledPin, ledState);   // Update the actual LED
+      Serial.printf("Turn on the LED \n");
     }
-  }
-};
+  } // End of Update() function
+}; //End of ledStrip Class
 
 
-Flasher led1(false);
+
+ledStrip led1(false,LED_BUILTIN);
 
 void setup()
 {
-led1.Setup(true,100,200);
+led1.Setup(true,1000,2000);
 }
 
 void loop()
