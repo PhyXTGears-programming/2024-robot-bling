b07ae49f66d9d0f559edb0a0f56ba713a3314265
Ian Blair <54004327+IanBlair07@users.noreply.github.com>
2024-03-23 12:34:17 -0400

    Add files via upload

--- spiTalk/spiTalk.ino
+++ spiTalk/spiTalk.ino
@@ -8,12 +8,19 @@
 #include "SPISlave.h"
 
 // Wiring:
-// Master RX  GP0 <-> GP11  Slave TX
-// Master CS  GP1 <-> GP9   Slave CS
-// Master CK  GP2 <-> GP10  Slave CK
-// Master TX  GP3 <-> GP8   Slave RX
+// RoboRio RX (1) GP0 <-> GP11  Pico TX (15)
+// RoboRio CS (2) GP1 <-> GP9   Pico CS (12)
+// RoboRio CK (4) GP2 <-> GP10  Pico CK (14)
+// RoboRio TX (5) GP3 <-> GP8   Pico RX (11)
 
 SPISettings spisettings(1000000, MSBFIRST, SPI_MODE0);
+
+void blinkLed() {
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(200);                       // wait for a second
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(200);                       // wait for a second
+}
 
 // SPI master
 void setup() {
@@ -22,12 +29,22 @@ void setup() {
   SPI.setSCK(2);
   SPI.setTX(3);
   SPI.begin(true);
-
+  pinMode(LED_BUILTIN, OUTPUT);
   delay(5000);
 }
 
+void msgOut(char localMsg){
+  char msg[15];
+  memset(msg, 0, sizeof(msg));
+  sprintf(msg, &localMsg);
+  SPI.beginTransaction(spisettings);
+  SPI.transfer(msg, sizeof(msg));
+  SPI.endTransaction();
+  blinkLed();
+}
 int transmits = 0;
 void loop() {
+/**
   char msg[42];
   memset(msg, 0, sizeof(msg));
   sprintf(msg, "What's up? This is transmission %d", transmits);
@@ -37,5 +54,8 @@ void loop() {
   SPI.endTransaction();
   Serial.printf("M-RECV: '%s'\n", msg);
   transmits++;
+  delay(5000);
+**/
+  msgOut('My Message Out');  
   delay(5000);
 }
